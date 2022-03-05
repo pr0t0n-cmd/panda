3d8b1638467293bf1a25576d4c00659a0d119ef2
j0-maker <giorgio.bruzzi1@gmail.com>
2022-03-05 12:19:39 +0100

    Blog style

--- src/lib/data/articles.json.js
+++ src/lib/data/articles.json.js
@@ -2,11 +2,17 @@ export let articles = {
     "le-basi-della-blockchain": {
         title: "Le basi della Blockchain",
         description: "lorem ipsum suca, lorem ipsum suca, lorem ipsum suca",
-        link: "le-basi-della-blockchain"
+        link: "le-basi-della-blockchain",
+        topImage: "favicon.png"
     },
     "le-basi-di-sto-cazzo": {
         title: "Le basi di sto cazzo",
         description: "lorem ipsum sucaaaaaaaa",
         link: "le-basi-di-sto-cazzo"
+    },
+    "le-basi-di-sto-cazzo-2": {
+        title: "Le basi di sto cazzo 2",
+        description: "lorem ipsum sucaaaaaaaa 2",
+        link: "le-basi-di-sto-cazzo-2"
     }
 }
