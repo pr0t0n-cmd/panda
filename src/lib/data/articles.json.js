9a36bcf30781e0b7dd957317fa6b2d32e4b6f45a
winterandrea <andrecasero@gmail.com>
2022-03-05 12:25:02 +0100

    andrea

--- src/lib/data/articles.json.js
+++ src/lib/data/articles.json.js
@@ -1,12 +1,13 @@
 export let articles = {
-    "le-basi-della-blockchain": {
-        title: "Le basi della Blockchain",
-        description: "lorem ipsum suca, lorem ipsum suca, lorem ipsum suca",
-        link: "le-basi-della-blockchain"
-    },
-    "le-basi-di-sto-cazzo": {
-        title: "Le basi di sto cazzo",
-        description: "lorem ipsum sucaaaaaaaa",
-        link: "le-basi-di-sto-cazzo"
-    }
-}
+	'le-basi-della-blockchain': {
+		title: 'Le basi della Blockchain',
+		description: 'lorem ipsum suca, lorem ipsum suca, lorem ipsum suca',
+		link: 'le-basi-della-blockchain',
+		topImage: 'favicon.png'
+	},
+	'le-basi-di-sto-cazzo': {
+		title: 'Le basi di sto cazzo',
+		description: 'lorem ipsum sucaaaaaaaa',
+		link: 'le-basi-di-sto-cazzo'
+	}
+};
