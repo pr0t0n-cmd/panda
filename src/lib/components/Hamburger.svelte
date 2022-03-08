705b23aa4bd5ae210e5f9cefa544b55ee89ffc45
winterandrea <andrecasero@gmail.com>
2022-03-08 16:24:42 +0100

    rr

--- src/lib/components/Hamburger.svelte
+++ src/lib/components/Hamburger.svelte
@@ -3,12 +3,21 @@
 </script>
 
 <button  class:open on:click={() => open = !open}>
-	<img src="icone/menu.svg" alt="hamburger" />
+	<img src="/icone/menu.svg" alt="hamburger" />
 </button>
 
 <style>
  img{
 	 width: 50px;
 	 background: transparent;
+ }
+
+ button{
+	     border: none;
+    background: transparent;
+		position: absolute;
+		top: 20px;
+		left: 20px;
+		
  }
 </style>
