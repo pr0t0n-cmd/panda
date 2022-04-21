c4ebc2736b2608a0088138fd4954f999fbe29348
j0-maker <giorgio.bruzzi1@gmail.com>
2022-04-21 15:13:43 +0200

    author emoji

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -18,7 +18,7 @@
         </div>
     </a>
 {:else}
-    <p>🤦‍♂️{content}<span class="author"> - 👨{author}</span></p>
+    <p>🤦‍♂️{content}<span class="author"> - 💂🏼‍♂️ {author}</span></p>
 {/if}
 <style>
 
