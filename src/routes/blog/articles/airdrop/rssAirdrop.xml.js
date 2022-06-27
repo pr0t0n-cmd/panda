0bc495c77c006cb29795920aa3be9ee54cea60c8
winterandrea <andrecasero@gmail.com>
2022-06-27 22:42:37 +0200

    airdrop rss

---
+++ src/routes/blog/articles/airdrop/rssAirdrop.xml.js
@@ -0,0 +1,31 @@
+import { articles } from '$lib/data/articles.json';
+
+export const get = async () => {
+	let items = '';
+	for (const [key, value] of Object.entries(articles)) {
+		if (value.category === 'airdrop') {
+			items += `<item>
+    <title>${value.title}</title>
+    <link>https://thecodingpanda.dev/blog/articles/airdrop/${value.link}</link>
+    <description>${value.description}</description>
+  </item>`;
+		}
+	}
+	const rssDocument = `
+<rss version="2.0">
+<channel>
+  <title>The Coding Panda</title>
+  <link>https://www.thecodingpanda.dev</link>
+  <description>Free web building tutorials</description>
+  ${items}
+</channel>
+
+</rss>`;
+	return {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/rss+xml'
+		},
+		body: rssDocument
+	};
+};
